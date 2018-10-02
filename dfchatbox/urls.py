3607a7180f97e191628f82b457dcfa29f96219c2
unknown <valter.hudovernik@gmail.com>
2018-10-02 14:52:20 +0200

    Removed unused code

--- dfchatbox/urls.py
+++ dfchatbox/urls.py
@@ -12,8 +12,5 @@ from . import views
 app_name = 'dfchatbox'
 urlpatterns = [
         path('',views.index,name='index'),
-        path('check_links',views.check_links,name='check_links'),
-        path('webhook',views.webhook,name='webhook'),
-        path('entry_tree/<data>',views.entry_tree,name='entry_tree'),
         path('updateDatabase/',views.update_db,name='update_db')
         ]
